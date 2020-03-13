edf25b48d09fd7c15ee59e70a4a380bfc9969d45
YAKOROLEVAZAMKA <56391132+YAKOROLEVAZAMKA@users.noreply.github.com>
2020-03-13 17:35:58 +0300

    3-4 lesson completed

---
+++ test 3.3.2/smoke_tests/login.py
@@ -0,0 +1,8 @@
+# номер 3
+def test_guest_can_login():
+    pass
+
+# номер 4
+class TestLogin(object):
+    def test_guest_should_see_login_link():
+    pass
---
+++ test 3.3.2/test_regression.py
@@ -0,0 +1,17 @@
+class TestLessonCreate():
+    # номер 5
+    def test_create_lesson():
+        pass
+
+    # номер 6
+    def user_with_lesson_can_create_lesson_from_navbar_test():
+        pass
+
+class CourseCreate():
+    # номер 7
+    def test_create_course():
+        pass
+
+# номер 8
+def test_guest_can_open_new_course():
+    pass
